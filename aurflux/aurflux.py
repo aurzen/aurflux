aefc46d3093a34addd3b04011bcba51fdf4da981
Zenith <z@zenith.dev>
2020-06-25 09:36:56 -0700

    aurflux 1.2.52

--- aurflux/aurflux.py
+++ aurflux/aurflux.py
@@ -147,48 +147,41 @@ def register_builtins(aurflux: Aurflux):
                 res = await exec_func(script, globals(), locals())
             except Exception as e:
                 res = re.sub(r'File ".*[\\/]([^\\/]+.py)"', r'File "\1"', traceback.format_exc(limit=1))
-        resp = (f"Ran in {t.elapsed * 1000:.2f} ms\n"
-                f"**IN**:\n"
-                f"```py\n{script}\n```\n"
-                f"**OUT**:\n"
-                f"```py\n{res}\n```")
-        # if len(resp) > 1900
 
-    return Response((f""
-                     f"Ran in {t.elapsed * 1000:.2f} ms\n"
-                     f"**IN**:\n"
-                     f"```py\n{script}\n```\n"
-                     f"**OUT**:\n"
-                     f"```py\n{res}\n```"), trashable=True)
+        return Response((f""
+                         f"Ran in {t.elapsed * 1000:.2f} ms\n"
+                         f"**IN**:\n"
+                         f"```py\n{script}\n```\n"
+                         f"**OUT**:\n"
+                         f"```py\n{res}\n```"), trashable=True)
 
+    @aurflux.commandeer(name="help", parsed=False)
+    async def get_help(ctx: MessageContext, help_target: ty.Optional[str]):
+        """
+        help [command_name]
+        :param ctx:
+        :param args:
+        :return:
+        """
+        configs = aurflux.CONFIG.of(ctx)
+        public_cmds = {name: command for name, command in aurflux.commands.items() if not command.private and name != "help"}
+        if not help_target:
+            help_embed = discord.Embed(title=f"{utils.EMOJIS['question']} Command Help", description=f"{configs['prefix']}help <command> for more info")
+            for cmd_name, command in public_cmds.items():
+                help_embed.add_field(name=cmd_name, value=f"{configs['prefix']}{command.short_usage}", inline=False)
 
-@aurflux.commandeer(name="help", parsed=False)
-async def get_help(ctx: MessageContext, help_target: ty.Optional[str]):
-    """
-    help [command_name]
-    :param ctx:
-    :param args:
-    :return:
-    """
-    configs = aurflux.CONFIG.of(ctx)
-    public_cmds = {name: command for name, command in aurflux.commands.items() if not command.private and name != "help"}
-    if not help_target:
-        help_embed = discord.Embed(title=f"{utils.EMOJIS['question']} Command Help", description=f"{configs['prefix']}help <command> for more info")
-        for cmd_name, command in public_cmds.items():
-            help_embed.add_field(name=cmd_name, value=f"{configs['prefix']}{command.short_usage}", inline=False)
-
-        return Response(
-            embed=help_embed
-        )
-    else:
-        if help_target not in public_cmds:
-            return Response(f"No command `{help_target}` to show help for", errored=True)
-        embed = discord.Embed(
-            title="\U00002754 Command Help",
-            description=f"Help for `{configs['prefix']}{help_target}`")
-        if public_cmds[help_target].argparser:
-            embed.add_field(name="Usage", value=public_cmds[help_target].long_usage)
-        return Response(embed=embed)
+            return Response(
+                embed=help_embed
+            )
+        else:
+            if help_target not in public_cmds:
+                return Response(f"No command `{help_target}` to show help for", errored=True)
+            embed = discord.Embed(
+                title="\U00002754 Command Help",
+                description=f"Help for `{configs['prefix']}{help_target}`")
+            if public_cmds[help_target].argparser:
+                embed.add_field(name="Usage", value=public_cmds[help_target].long_usage)
+            return Response(embed=embed)
 
 
 class Aurflux(discord.Client):
