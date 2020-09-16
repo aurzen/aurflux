78e8c1c69e470fa67a8a638f03b5a012f61e0641
Zenith <z@zenith.dev>
2020-09-16 15:06:12 -0700

    aurflux 2.1.11

--- aurflux/__init__.py
+++ aurflux/__init__.py
@@ -1,6 +1,6 @@
 from .flux import FluxClient, FluxEvent
-from config import Config
-from errors import *
+from .config import Config
+from .errors import *
 
 __package__ = "aurflux"
 __all__ = ["FluxClient", "FluxEvent"]
