b55179583e8d492f125c850adf4ce0a9f9d6eb32
Zenith <z@zenith.dev>
2020-09-18 10:55:16 -0700

    aurflux 3.0.12

--- aurflux/command/command.py
+++ aurflux/command/command.py
@@ -67,8 +67,8 @@ class Command(aur.util.AutoRepr, AuthAware):
       except ValueError as e:
          raise ValueError(f"{e} : {self.name}")
       self.short_usage = short_usage.strip()
-      self.description = long_usage.strip()
-      self.param_usage: ty.List[ty.List[str]] = [param_line.split(":") for param_line in params.strip().split("\n")]
+      self.description = long_usage.strip().replace("\\n","\n")
+      self.param_usage: ty.List[ty.List[str]] = [param_line.replace("\\n","\n").split(":") for param_line in params.strip().split("\n")]
       # else:
       #    self.short_usage: str = func_doc.split("\n")[0]
       #    raw_doc = func_doc[func_doc.index("\n"):func_doc.index(":param")].strip()
