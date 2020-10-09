81d1a147e0c4f12526f56071eb0e73956e9e8508
Zenith <z@zenith.dev>
2020-10-09 10:40:21 -0700

    aurflux 3.2.1

--- aurflux/types_.py
+++ aurflux/types_.py
@@ -6,20 +6,20 @@ if ty.TYPE_CHECKING:
    from .flux import FluxEvent
 
 
-class CommandFunc(ty.Protocol):
-   def __call__(self, msg_ctx: GuildMessageCtx, auth_ctx: ty.Optional[AuthAwareCtx] = None, cmd_args: str = None, **kwargs): ...
+   class CommandFunc(ty.Protocol):
+      def __call__(self, msg_ctx: GuildMessageCtx, auth_ctx: ty.Optional[AuthAwareCtx] = None, cmd_args: str = None, **kwargs): ...
 
 
-ExtraCtxs: ty.TypeAlias = ty.Literal["auth"]
+   ExtraCtxs: ty.TypeAlias = ty.Literal["auth"]
 
 
-class GuildCommandCtx:
-   msg_ctx: GuildMessageCtx
-   author_ctx: AuthorAwareCtx
-   auth_ctxs: ty.List[AuthAwareCtx]
+   class GuildCommandCtx:
+      msg_ctx: GuildMessageCtx
+      author_ctx: AuthorAwareCtx
+      auth_ctxs: ty.List[AuthAwareCtx]
 
 
-class GuildCommandEvent(FluxEvent):
-   cmd_name: str
-   cmd_ctx: GuildCommandCtx
-   cmd_args: ty.Optional[str]
+   class GuildCommandEvent(FluxEvent):
+      cmd_name: str
+      cmd_ctx: GuildCommandCtx
+      cmd_args: ty.Optional[str]
