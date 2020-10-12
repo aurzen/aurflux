79b40faea563dd5bac2166dc16be2502e2ffd121
Zenith <z@zenith.dev>
2020-10-12 13:04:24 -0700

    aurflux 3.2.25

--- aurflux/utils/exec_.py
+++ aurflux/utils/exec_.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
 import ast
+
+
 async def sexec(script: str, globals_=None, locals_=None):
    exec_context = {**globals_, **locals_}
-
    stmts = list(ast.iter_child_nodes(ast.parse(script)))
    if not stmts:
       return None
    if isinstance(stmts[-1], ast.Expr):
       if len(stmts) > 1:
+         # noinspection PyArgumentList
          exec(compile(ast.Module(body=stmts[:-1], type_ignores=[]), filename="<ast>", mode="exec"), exec_context)
-      return eval(compile(ast.Expression(expr_body=stmts[-1].value), filename="<ast>", mode="eval"), exec_context)
+      # noinspection PyArgumentList
+      return eval(compile(ast.Expression(body=stmts[-1].value), filename="<ast>", mode="eval"), exec_context)
    else:
       exec(script, globals_, locals_)
 
@@ -19,6 +22,6 @@ async def aexec(script: str, globals_=None, locals_=None):
    exec_context = {**globals_, **locals_}
    exec(
       f'async def __ex(): ' +
-      ''.join(f'\n {l}' for l in script.split('\n')), exec_context
+      ''.join(f'\n {li}' for li in script.split('\n')), exec_context
    )
    return await exec_context['__ex']()
