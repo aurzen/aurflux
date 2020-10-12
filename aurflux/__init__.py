be22f4563f7002da9032faf0a43cfbdde26990e9
Zenith <z@zenith.dev>
2020-10-12 14:39:58 -0700

    aurflux 3.2.31

--- aurflux/__init__.py
+++ aurflux/__init__.py
@@ -6,6 +6,5 @@ from .config import Config
 from .context import CommandCtx
 from .errors import CommandError
 from .flux import CommandEvent, FluxClient, FluxEvent
-from .types_ import GuildCommandCtx
 
 __all__ = ["FluxClient", "FluxEvent", "CommandEvent", "Config", "errors", "utils", "context", "cog", "command", "ty", "FluxCog","CommandCtx","GuildCommandCtx","CommandError"]
