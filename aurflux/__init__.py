7d2bc327f16b82665909af76982d9680c3a48d80
Zenith <z@zenith.dev>
2020-10-12 13:40:56 -0700

    aurflux 3.2.29

--- aurflux/__init__.py
+++ aurflux/__init__.py
@@ -1,11 +1,11 @@
 __package__ = "aurflux"
 
-from . import types_ as ty
-from . import cog, command, context, errors, utils
-from .config import Config
-from .flux import CommandEvent, FluxClient, FluxEvent
+from . import cog, command, context, errors, types_ as ty, utils
 from .cog import FluxCog
+from .config import Config
 from .context import CommandCtx
+from .errors import CommandError
+from .flux import CommandEvent, FluxClient, FluxEvent
 from .types_ import GuildCommandCtx
 
-__all__ = ["FluxClient", "FluxEvent", "CommandEvent", "Config", "errors", "utils", "context", "cog", "command", "ty"]
+__all__ = ["FluxClient", "FluxEvent", "CommandEvent", "Config", "errors", "utils", "context", "cog", "command", "ty", "FluxCog","CommandCtx","GuildCommandCtx","CommandError"]
